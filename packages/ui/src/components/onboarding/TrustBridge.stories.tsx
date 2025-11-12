7a4282f8717eb16815ec28b25174cdcf972d8f9f
Milad Arbabi <arbabi.milad@gmail.com>
2025-11-12 18:25:31 +0100

    feat(onboarding): create TrustBridge component (#711)

---
+++ packages/ui/src/components/onboarding/TrustBridge.stories.tsx
@@ -0,0 +1,24 @@
+// packages/ui/src/components/onboarding/TrustBridge.stories.tsx
+import { Meta, StoryObj } from '@storybook/react';
+import { TrustBridge } from './TrustBridge';
+
+const meta: Meta<typeof TrustBridge> = {
+  title: 'Onboarding/TrustBridge',
+  component: TrustBridge,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => (
+    <div style={{ width: '400px' }}>
+      <TrustBridge />
+    </div>
+  ),
+};
